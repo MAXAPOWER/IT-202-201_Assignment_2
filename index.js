52bbfa5c8f3e84056a3e43b3e4c1d73b32c3cfb6
Max <MSoler01@gmail.com>
2025-10-18 02:04:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ function reverseString() {
   var userInput = prompt("Please enter a string: ");
   var reverseUserInput = userInput.split("").reverse().join("");
   alert("Your string, " + userInput + ", reversed is: " + reverseUserInput);
+  document.getElementById("reverseString");
 };
 
 reverseString();
